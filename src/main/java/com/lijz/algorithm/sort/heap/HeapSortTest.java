57d33fbc2a58b0124a0dabcb7ea58f183cc3b430
李建忠 <lijianzhong@meituan.com>
2015-12-13 22:41:34 +0800

    add insert and delete

--- src/main/java/com/lijz/algorithm/sort/heap/HeapSortTest.java
+++ src/main/java/com/lijz/algorithm/sort/heap/HeapSortTest.java
@@ -1,5 +1,7 @@
 package com.lijz.algorithm.sort.heap;
 
+import java.util.Arrays;
+
 /**
  * Created by jianzhongli on 15/12/13.
  */
@@ -13,7 +15,10 @@ public class HeapSortTest {
         //test.maxHeapify(array,2);
         //test.minHeapify(array, 0);
         //test.buildMaxHeap(array, array.length);
-        test.heapSort(array);
+        //test.heapSort(array);
+        //test.increaseHeapKey(array, 5, 30);
+        //array = test.insert(array, 50);
+        array = test.delete(array, 5);
         for(int i = 0; i<array.length ; i++){
             System.out.println(array[i]);
         }
@@ -101,5 +106,46 @@ public class HeapSortTest {
             array[minIdx] = tmp;
             minHeapify(array, minIdx);
         }
+    }
+
+    public void increaseHeapKey(int array[], int i, int key){
+
+        if(key < array[i]){
+            return ;
+        }
+
+        array[i] = key;
+        int j=(i-1)/2;
+        while(j>=0 && array[i] > array[j]){
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+            i = j;
+            j = (j-1)/2;
+        }
+    }
+
+    public int[] insert(int array[], int key){
+
+        int newArray[] = Arrays.copyOf(array, array.length+1);
+        newArray[newArray.length -1] = Integer.MIN_VALUE;
+        increaseHeapKey(newArray, newArray.length-1, key);
+        return newArray;
+    }
+
+    public int[] delete(int array[], int i){
+
+        if(i<0 || i>=array.length){
+            return array;
+        }
+
+        array[i] = array[array.length-1];
+
+        int[] newArray = new int[array.length-1];
+        System.arraycopy(array, 0 , newArray, 0, newArray.length);
+        maxHeapify(newArray, i, newArray.length);
+
+        return newArray;
+
     }
 }
